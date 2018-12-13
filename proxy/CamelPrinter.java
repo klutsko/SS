d7695a2fd11ba2b8c506b4dbf8a3b96ca18b4e91
kseniialutsko <xenija.vasina@gmail.com>
2018-12-13 14:17:07 +0200

    changes 1

--- proxy/CamelPrinter.java
+++ proxy/CamelPrinter.java
@@ -1,16 +1,14 @@
 package homework.task;
 
-public class CamelPrinter implements Printable {
-    Printer printer;
+public class CamelPrinter  {
 
-    public CamelPrinter() {
 
-    }
+
+
 
     @Override
     public String print(String inputString) {
         inputString = inputString.replaceAll(" ", "");
-        System.out.println(inputString);
         return inputString;
     }
 
